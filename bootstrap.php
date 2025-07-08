bfbc80d4f9010f49bbdda02e3661fe30a1c38caf
MarkGamboa <202310110@fit.edu.ph>
2025-07-08 11:51:42 +0800

    feat: Add path definitions for components, layouts, and assets in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,8 @@ define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
 define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
+define('COMPONENTS_PATH', realpath(BASE_PATH . "/components"));
+define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
+define('ASSETS_PATH', realpath(BASE_PATH . "/assets"));
 
 chdir(BASE_PATH);
